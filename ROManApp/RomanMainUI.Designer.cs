35fc7fbeae45a7e15c925670819d24e912aa345a
Aymen Furter <aymen.furter+github@gmail.com>
2019-09-01 20:29:39 +0200

    Initial Commit

---
+++ ROManApp/RomanMainUI.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ROManApplication
+{
+    partial class RomanMainUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RomanMainUI));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Refresh Processes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.refresh);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(288, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Switch to Character";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(228, 47);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Shutdown All RO Processes";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.killAll);
+            // 
+            // RomanMainUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 384);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RomanMainUI";
+            this.Text = "ROMan - Ragnarok Online Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ROManApp/RomanMainUI.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+
+namespace ROManApplication
+{
+    public partial class RomanMainUI : Form
+    {
+        Process[] processListLastRefreshed = null;
+        ArrayList processItems = new ArrayList();
+
+        RomanMainUI form = null;
+        public RomanMainUI()
+        {
+            InitializeComponent();
+            form = this;
+        }
+
+
+        void imageboxClickFocus(object sender, EventArgs e)
+        {
+            PictureBox box = (PictureBox)sender;
+
+            int activationIndex = 0;
+            foreach (ProcessItem theprocess in processItems)
+            {
+
+                if (theprocess.tag.Equals(box.Text))
+                {
+                    IntPtr p = theprocess.process.MainWindowHandle;
+                    SetForegroundWindow(p);
+                }
+                activationIndex = activationIndex + 1;
+            }
+        }
+
+        private void refresh(object sender, EventArgs e)
+        {
+            processListLastRefreshed = Process.GetProcesses();
+            Process mainProcess = Process.GetCurrentProcess();
+            IntPtr mainP = mainProcess.MainWindowHandle;
+            int processID = 0;
+            foreach (Process theprocess in processListLastRefreshed)
+            {
+                if (theprocess.ProcessName.Contains("Ragexe") || theprocess.ProcessName.Contains("iro"))
+                {
+                    processID = processID + 1;
+                    ProcessItem item = new ProcessItem();
+                    item.process = theprocess;
+                    item.Name = theprocess.ProcessName + " " + processID;
+                    item.DisplayMember = theprocess.ProcessName + " " + processID;
+                    item.tag = processID.ToString();
+
+                    IntPtr p = theprocess.MainWindowHandle;
+                    SetForegroundWindow(p);
+                    System.Threading.Thread.Sleep(100);
+                    var proc = theprocess;
+                    var rect = new User32.Rect();
+                    User32.GetWindowRect(proc.MainWindowHandle, ref rect);
+
+                    int width = 200;
+                    int height = 50;
+
+                    var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                    Graphics graphics = Graphics.FromImage(bmp);
+                    graphics.CopyFromScreen(rect.left + 5, rect.top + 28, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+                    {
+                        PictureBox P = new PictureBox();
+                    
+                        P.Image = bmp;
+                        P.Size = new Size(250, 50);
+                        P.Text = processID.ToString();
+
+                        P.Location = new System.Drawing.Point(300, ((processID-1) * 60) + 30);
+                        form.Controls.Add(P);
+                        P.Parent = form;
+                        P.MouseClick += new MouseEventHandler(imageboxClickFocus);
+                    }
+
+                    processItems.Add(item);
+                }
+                
+            }
+            SetForegroundWindow(mainP);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+  
+        private void killAll(object sender, EventArgs e)
+        {
+            foreach (ProcessItem theprocess in processItems)
+            {
+                try
+                {
+                    theprocess.process.Kill();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+    class ProcessItem
+    {
+        public Process process;
+        public String Name { get; set; }
+        public String DisplayMember { get; set; }
+        public String tag { get; set; }
+    }
+
+    internal static class NativeMethods
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(IntPtr hWnd, int Msg, Keys wParam, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+    }
+
+    public class User32
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+    }
+
+}
